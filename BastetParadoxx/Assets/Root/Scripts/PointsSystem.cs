62d884cdd2013f7426c4ef8bc4a474c62dc47eed
NadyaNM <nadya11nn2005@gmail.com>
2025-02-18 00:24:45 +0100

    recogerpickups

---
+++ BastetParadoxx/Assets/Root/Scripts/PointsSystem.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointsSystem : MonoBehaviour
+{
+    [SerializeField] int actualPoints;
+    [SerializeField] int winPoints;
+    [SerializeField] GameObject winCoin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        actualPoints = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (actualPoints >= winPoints) { winCoin.SetActive(true); }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("PickUp"))
+        {
+            actualPoints += 1;
+            collision.gameObject.SetActive(false);
+        }
+    }
+}
+
---
+++ BastetParadoxx/Assets/Root/Scripts/WinTrigger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinTrigger : MonoBehaviour
+{
+    [SerializeField] int sceneToLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+
+}
