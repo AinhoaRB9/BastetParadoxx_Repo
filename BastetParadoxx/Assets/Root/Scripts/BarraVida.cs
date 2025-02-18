72ba98a17a44a3413ef5a28d4f35532035f5194a
NadyaNM <nadya11nn2005@gmail.com>
2025-02-19 00:48:37 +0100

    barra vida bien

--- BastetParadoxx/Assets/Root/Scripts/BarraVida.cs
+++ BastetParadoxx/Assets/Root/Scripts/BarraVida.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class BarraVida : MonoBehaviour
 {
+    public Image rellenoBarraVida;
+    private PlayerController playerController;
+    private float vidaMaxima;
+   
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+        vidaMaxima = playerController.maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rellenoBarraVida.fillAmount = playerController.maxHealth / vidaMaxima;
     }
 }
